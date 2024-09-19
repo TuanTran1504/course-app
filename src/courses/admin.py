cd5a5a462b3891b9f2e8bc7653e7aa1f673ad617
TuanTran1504 <tran.dinhtuan1542@gmail.com>
2024-09-19 17:46:48 +1000

    Ordering Model + Add lessons

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 # Register your models here.
+from cloudinary import CloudinaryImage
+from .models import Course, Lesson
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    readonly_fields = [
+        'updated'
+    ]
+    extra = 0
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    fields = ['title', 'description', 'status', 'image', 'access', 'display_image']
+    list_filter = ['status', 'access']
+    readonly_fields = ['display_image']
+    list_display = ['title', 'status', 'access']
+    def display_image(self, obj, *args, **kwargs):
+        # url = helpers.get_cloudinary_image_object(
+        #     obj, 
+        #     field_name='image',
+        #     width=200
+        # )
+        url = obj.image_admin
+        return format_html(f"<img src={url} />")
+    
+    display_image.short_description = "Current Image"
+# admin.site.register(Course)
--- src/courses/models.py
+++ src/courses/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+import helpers
+from cloudinary.models import CloudinaryField
 # Create your models here.
-
+helpers.cloudinary_init()
 class PublishStatus(models.TextChoices):
     PUBLISHED = "pub" , 'Published'
     COMING_SOON = "soon", "Coming Soon"
@@ -19,14 +20,14 @@ class AccessRequirement(models.TextChoices):
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
-    # image = CloudinaryField(
-    #     "image", 
-    #     null=True, 
-    #     public_id_prefix=get_public_id_prefix,
-    #     display_name=get_display_name,
-    #     tags=["course", "thumbnail"]
-    # )
+    # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField(
+        "image", 
+        null=True, 
+        # public_id_prefix=get_public_id_prefix,
+        # display_name=get_display_name,
+        # tags=["course", "thumbnail"]
+    )
     access = models.CharField(
         max_length=5,
         choices=AccessRequirement.choices,
@@ -37,6 +38,59 @@ class Course(models.Model):
         choices= PublishStatus.choices,
         default = PublishStatus.DRAFT
         )
+    
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+    
+    @property
+    def image_admin(self):
+        if not self.image:
+            return ''
+        image_options = {
+            "width" : 200
+        }
+        url = self.image.build_url(**image_options)
+        return url
+    def image_thumbnail(self, as_html=False, width=500):
+        if not self.image:
+            return ''
+        image_options = {
+            "width" : 200
+        }
+        if as_html:
+            return self.image.image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    # course_id 
+    public_id = models.CharField(max_length=130, blank=True, null=True, db_index=True)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    preview = models.BooleanField(default=False,help_text="If user does not have access to course, can they see this?" )
+    # thumbnail = CloudinaryField("image", 
+    #             public_id_prefix=get_public_id_prefix,
+    #             display_name=get_display_name,
+    #             tags = ['thumbnail', 'lesson'],
+    #             blank=True, null=True)
+
+    video = CloudinaryField("video",null=True, blank=True,resource_type='video') 
+    thumbnail = CloudinaryField("image", null=True, blank=True)
+    
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.PUBLISHED
+    )
+    order = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order','-updated']
+    
