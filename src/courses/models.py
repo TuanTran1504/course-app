882da6f4d66045c2542fc52b3c5e25957c156cb3
TuanTran1504 <tran.dinhtuan1542@gmail.com>
2024-09-19 15:42:46 +1000

    Adjust migration

--- src/courses/models.py
+++ src/courses/models.py
@@ -3,23 +3,34 @@ from django.db import models
 # Create your models here.
 
 class PublishStatus(models.TextChoices):
-    ANYONE = "any" , 'Anyone'
-    EMAIL_REQUIRED = "email_required", "Email required"
-    
-
-class AccessRequirement(models.TextChoices):
     PUBLISHED = "pub" , 'Published'
     COMING_SOON = "soon", "Coming Soon"
     DRAFT = "draft", "Draft"
 
+def handle_upload(instance, filename):
+    return f"{filename}"
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any" , 'Anyone'
+    EMAIL_REQUIRED = "email", "Email required"
+ 
+    
+
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    # image
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    # image = CloudinaryField(
+    #     "image", 
+    #     null=True, 
+    #     public_id_prefix=get_public_id_prefix,
+    #     display_name=get_display_name,
+    #     tags=["course", "thumbnail"]
+    # )
     access = models.CharField(
-        max_length=10,
+        max_length=5,
         choices=AccessRequirement.choices,
-        default=AccessRequirement.DRAFT
+        default=AccessRequirement.EMAIL_REQUIRED
     )
     status = models.CharField(
         max_length=10,
